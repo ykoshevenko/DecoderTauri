464d59a05a71d32b70bd2250b07da1c49a99813b
ykoshevenko <ykoshevenko@list.ru>
2025-10-27 14:03:46 +0300

    debagging

--- server/app/haffman/index_haff.py
+++ server/app/haffman/index_haff.py
@@ -35,26 +35,32 @@ class newTrees: #создание дерева программного
         self.codes = {}
 
     def addVertex(self):
+        if len(self.obj) < 2:
+            return None
+        
+        small_items = sorted(self.obj.items(), key=lambda x: x[1])[:2] #находим 2 минимальных  значения
+
+        for key, _ in small_items:
+            del self.obj[key]
+
+        new_key = f'({small_items[0][0]} + {small_items[1][0]})'
+        new_value = small_items[0][1] + small_items[1][1]
+
+        self.obj[new_key] = new_value
+
+        self.tree.add_child(new_key, small_items[0][0], position=1)
+        self.tree.add_child(new_key, small_items[1][0], position=0)
+
+    def build_tree(self):
         while len(self.obj) > 1:
-            small_items = sorted(self.obj.items(), key=lambda x: x[1])[:2] #находим 2 минимальных  значения
+            self.addVertex()
 
-            for key, _ in small_items:
-                del self.obj[key]
-
-            new_key = f'({small_items[0][0]} + {small_items[1][0]})'
-            new_value = small_items[0][1] + small_items[1][1]
-
-            self.obj[new_key] = new_value
-
-            self.tree.add_child(new_key, small_items[0][0], position=1)
-            self.tree.add_child(new_key, small_items[1][0], position=0)
-
-        return self.tree
+        return list(self.obj.keys())[0] if self.obj else None
     
-    def generate_codes(self, node, current_codes):
+    def generate_codes(self, node, current_codes=''):
         if node in self.tree.graph and self.tree.graph[node]:
             for child in self.tree.graph[node]:
                 bit = str(self.tree.positions[child])
                 self.generate_codes(child, current_codes+bit)
-            else:
-                self.codes[node] = current_codes
+        else:
+            self.codes[node] = current_codes
